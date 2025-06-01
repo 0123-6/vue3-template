f1e9b36798769e1b64dff3379d8b22a5c2dc50bf
90945 <909458209@qq.com>
2025-06-01 16:05:22 +0800

    优化用户类型定义

--- src/views/system-manage/user-manage/userManageCommon.ts
+++ src/views/system-manage/user-manage/userManageCommon.ts
@@ -2,28 +2,24 @@ import {ISelectOption} from "@/components/base-form/useElSelect.ts";
 
 // 用户的信息
 export interface IUserInfo {
-	// 系统级别基本信息,必须存在
-	// 系统的唯一标识
-	id: string,
-	// 用户名,不可重复
+	// 账号,系统的唯一标识,不可重复
 	account: string,
-	// 密码
-	password: string,
-	// 状态
-	status: 'normal' | 'stop',
-
-	// 用户选填的一些个人信息
-	phone?: string,
-	// 邮箱
-	email?: string,
+	// 昵称
+	nickname?: string,
 	// 性别
 	sex?: 'man' | 'woman',
-	// 年龄
-	age?: number,
+	// 手机号
+	phone?: string,
+	// 状态
+	status: 'normal' | 'stop',
+	// 简介
+	description?: string,
+	// 创建时间
+	createTime?: string,
 
 	// 权限相关,分为前端权限+后端权限
 	// 前端权限
-	roleList: string[],
+	roleList?: string[],
 }
 
 export const sexList: ISelectOption[] = [
