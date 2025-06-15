d0cb27ad1c7d1ed604c0de3ce13df63cf84c0495
90945 <909458209@qq.com>
2025-06-15 15:49:36 +0800

    useElTable.ts优化

--- src/components/base-table/useElTable.ts
+++ src/components/base-table/useElTable.ts
@@ -1,27 +1,20 @@
 import {useTemplateRef} from "vue";
 import {ElMessage, TableInstance} from "element-plus";
-import {IBaseFetch} from "@/util/api.ts";
-import {useBaseFetch} from "@/util/hooks/useBaseFetch.ts";
+import {formatterValue, generateMockObject, IBaseFetch, IBaseItem} from "@/util/api.ts";
+import {IUseBaseFetchReturn, useBaseFetch} from "@/util/hooks/useBaseFetch.ts";
 import {camelToSnake} from "@/util/stringUtil.ts";
 import {useResetReactive, useResetRef} from "@/util/hooks/useResetState.ts";
-import {ISelectOption} from "@/components/base-form/useElSelect.ts";
 
-export interface IBaseTableColumn {
+export interface IBaseTableColumn extends IBaseItem{
 	type?: 'selection',
-	prop?: string,
-	label?: string,
-	formatter?: (value: string | number) => (string | number)
 	width?: number,
 	minWidth?: number,
 	align?: 'left' | 'center' | 'right',
 	fixed?: 'left' | 'right',
 	children?: IBaseTableColumn[],
 	hidden?: boolean | (() => boolean),
-	// 适用于{label: '', value: ''}类固定信息展示
-	list?: ISelectOption[],
 	// 操作列
 	operatorList?: IOperatorItem[],
-	mock?: any | any[],
 }
 
 export interface IOperatorItem {
@@ -40,7 +33,11 @@ export interface IUseElTableProps {
 	// 分页
 	pageSizeList?: number[],
 	// 因为这是要多次执行的,所以不能传递一个一次性值,而是一个函数,获取当时的期待值
-	fetchOptionFn: () => IBaseFetch,
+	// 通过接口获取数据时必填
+	fetchOptionFn?: () => IBaseFetch,
+	// 现成的数据
+	preparedData?: any[],
+	// 列属性数组,支持嵌套1次(二维数组)
 	list: IBaseTableColumn[],
 }
 
@@ -86,42 +83,10 @@ export const useElTable = (props: IUseElTableProps)
 		pageSizeList = [10, 20, 30],
 		fetchOptionFn,
 		list,
+		preparedData,
 	} = props
 	const refName = props.ref ?? 'tableRef'
 	const tableRef = useTemplateRef<TableInstance>(refName)
-	const listMap = Object.create(null)
-	const mockObject = Object.create(null)
-	const dfsList = (item: IBaseTableColumn) => {
-		if (!item) {
-			return
-		}
-		if (Array.isArray(item.children)) {
-			// 嵌套column
-			item.children.forEach(dfsList)
-			return
-		}
-		// 单个column
-		if (!(item.label && item.prop)) {
-			return
-		}
-		// 设置listMap
-		listMap[item.prop] = item
-		// {label: '', value: ''}格式
-		if (item.list?.length) {
-			mockObject[item.prop] = [
-				...(item.list.map(_item => _item.value)),
-				'',
-			]
-			return
-		}
-		mockObject[item.prop] = item.mock ??
-			[
-				item.label,
-				'',
-				Array(20).fill(item.label).join(),
-			]
-	}
-	list.forEach(dfsList)
 
 	const {
 		state: params,
@@ -170,52 +135,73 @@ export const useElTable = (props: IUseElTableProps)
 		list: [],
 	}))
 
-	const fetchTable = useBaseFetch({
-		beforeFetchResetFn: () => reset(true),
-		fetchOptionFn: () => ({
-			...fetchOptionFn(),
-			mockUrl: 'getTableList',
-			mockObject,
-			data: {
-				...params,
-				...(fetchOptionFn().data),
-			},
-		}),
-		transformResponseDataFn: (responseData: any) => {
-			if (!responseData) {
-				resetData()
-				return
-			}
-			if (typeof responseData !== 'object') {
-				ElMessage.warning('表格请求接口返回值类型不合法,请检查接口')
-				resetData()
-				return
-			}
-			if (responseData.total == null || responseData.list == null || responseData.total < 0) {
-				resetData()
-				return
-			}
-			if (!Number.isInteger(responseData.total) || !Array.isArray(responseData.list)) {
-				ElMessage.warning('表格请求接口返回值类型不合法,请检查接口')
-				resetData()
-				return
-			}
+	const solveRawData = (responseData: any) => {
+		if (!responseData) {
+			resetData()
+			return
+		}
+		if (typeof responseData !== 'object') {
+			ElMessage.warning('表格请求接口返回值类型不合法,请检查接口')
+			resetData()
+			return
+		}
+		if (responseData.total == null || responseData.list == null || responseData.total < 0) {
+			resetData()
+			return
+		}
+		if (!Number.isInteger(responseData.total) || !Array.isArray(responseData.list)) {
+			ElMessage.warning('表格请求接口返回值类型不合法,请检查接口')
+			resetData()
+			return
+		}
 
-			responseData.list = (responseData.list as Array<Record<string, any>>)
-				.filter(Boolean)
-				.map((item, index) => ({
-					...Object.fromEntries(
-						Object.entries(item).map(([key, value]) => [
-							key,
-							listMap[key]?.formatter?.(value) ?? value,
-						])
-					),
-					index: index + 1 + params.pageSize * (params.pageNum - 1)
-				}))
-			resetData(responseData)
-		},
-		microTask: true,
-	})
+		responseData.list = (responseData.list as Array<Record<string, any>>)
+			.filter(Boolean)
+			.map((item, index) => ({
+				...formatterValue(item, list),
+				index: index + 1 + params.pageSize * (params.pageNum - 1)
+			}))
+		resetData(responseData)
+	}
+
+	let fetchTable: IUseBaseFetchReturn
+	if (fetchOptionFn) {
+		fetchTable = useBaseFetch({
+			beforeFetchResetFn: () => reset(true),
+			fetchOptionFn: () => ({
+				...fetchOptionFn(),
+				mockUrl: 'getTableList',
+				mockObject: generateMockObject(list),
+				data: {
+					...params,
+					...(fetchOptionFn().data),
+				},
+			}),
+			transformResponseDataFn: solveRawData,
+			microTask: true,
+		})
+	} else {
+		fetchTable = {
+			beforeFetchHookSet: undefined,
+			isFetching: false,
+			beforeFetch: () => {},
+			doFetch: async ()
+				: Promise<boolean> => {
+				return new Promise((resolve, _reject) => {
+					// 需要等待dom结构出来后再渲染数据,否则组件判断文字是否溢出有bug,永远为溢出.
+					setTimeout(() => {
+						reset(true)
+						solveRawData({
+							list: preparedData,
+							total: preparedData.length,
+						})
+						resolve(true)
+					}, 0)
+				})
+			},
+		}
+		fetchTable.doFetch()
+	}
 
 	// 排序
 	const changeSort = async ({prop, order = ''}) => {
@@ -253,6 +239,8 @@ export const useElTable = (props: IUseElTableProps)
 			resetSelectItemList()
 		} else if (newValue === 'batch') {
 			resetInnerType('batch')
+			// 单个选择是即时生效,不缓存
+			resetSelectItem()
 		} else {
 			resetSelectItem(newValue)
 			resetInnerType('single')
