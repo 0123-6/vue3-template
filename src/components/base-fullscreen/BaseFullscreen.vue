a35513ecc38482683938b5e758ec4cf69386073e
90945 <909458209@qq.com>
2025-07-13 11:32:19 +0800

    适配eslint

--- src/components/base-fullscreen/BaseFullscreen.vue
+++ src/components/base-fullscreen/BaseFullscreen.vue
@@ -7,10 +7,11 @@ const fullscreenObject: IUseFullscreenReturn = useFullscreen()
 </script>
 
 <template>
-	<button class="w-[32px] h-[32px] rounded-full hover:bg-disabled flex justify-center items-center"
-					@click="fullscreenObject.isFullScreen = !fullscreenObject.isFullScreen"
-	>
-		<full-screen-icon v-show="!fullscreenObject.isFullScreen"/>
-		<not-full-screen-icon v-show="fullscreenObject.isFullScreen"/>
-	</button>
+  <button
+    class="w-[32px] h-[32px] rounded-full hover:bg-disabled flex justify-center items-center"
+    @click="fullscreenObject.isFullScreen = !fullscreenObject.isFullScreen"
+  >
+    <full-screen-icon v-show="!fullscreenObject.isFullScreen" />
+    <not-full-screen-icon v-show="fullscreenObject.isFullScreen" />
+  </button>
 </template>
