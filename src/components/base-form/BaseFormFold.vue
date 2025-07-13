4e398f956a3c956739105366ef018d7e7a07ce7e
90945 <909458209@qq.com>
2025-07-13 10:03:36 +0800

    适配eslint

--- src/components/base-form/BaseFormFold.vue
+++ src/components/base-form/BaseFormFold.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {IUseElFormReturn} from "@/components/base-form/useElForm.ts";
+import {ArrowDown, ArrowUp} from "@element-plus/icons-vue";
 
 interface IProps {
 	formObject: IUseElFormReturn,
@@ -12,14 +13,15 @@ const changeFold = () => {
 </script>
 
 <template>
-	<button v-if="formObject.canFold"
-					@click="changeFold"
-					class="hpj text-primary flex items-center gap-x-1"
-	>
-		<span>{{!formObject.isFold.value ? '收起' : '展开'}}</span>
-		<el-icon>
-			<ArrowUp v-show="!formObject.isFold.value"/>
-			<ArrowDown v-show="formObject.isFold.value"/>
-		</el-icon>
-	</button>
+  <button
+    v-if="formObject.canFold"
+    class="hpj text-primary flex items-center gap-x-1"
+    @click="changeFold"
+  >
+    <span>{{ !formObject.isFold.value ? '收起' : '展开' }}</span>
+    <el-icon>
+      <ArrowUp v-show="!formObject.isFold.value" />
+      <ArrowDown v-show="formObject.isFold.value" />
+    </el-icon>
+  </button>
 </template>
