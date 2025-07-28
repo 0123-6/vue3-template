8182e8a88f389283360085f56d32b564ca70d850
90945 <909458209@qq.com>
2025-07-28 18:58:31 +0800

    添加获取用户信息接口

--- src/plugin/pinia.ts
+++ src/plugin/pinia.ts
@@ -1,37 +1,50 @@
 import {ref} from 'vue'
 import {defineStore} from 'pinia'
-import {IUserInfo} from "@views/system-manage/user-manage/userManageCommon.ts";
+import {IUserInfo} from '@views/system-manage/user-manage/userManageCommon.ts'
+import {useBaseFetch} from '@/util/hooks/useBaseFetch.ts'
 
 export const useUserStore = defineStore('user', () => {
-	const user = ref(null as IUserInfo)
+  const user = ref(null as IUserInfo)
 
-	return {
-		user,
-	}
+  return {
+    user,
+  }
 })
 
 // 保存全局权限
 export const usePermissionsStore = defineStore('permissions', () => {
-	// 按钮权限数组
-	const permissions = ref([])
-	// 是否有某个权限
-	const isPermission = permission => {
-		return permissions.value.includes(permission) || import.meta.env.DEV
-	}
+  // 按钮权限数组
+  const permissions = ref([])
+  // 是否有某个权限
+  const isPermission = permission => {
+    return permissions.value.includes(permission) || import.meta.env.DEV
+  }
 
-	return {
-		permissions,
-		isPermission,
-	}
+  return {
+    permissions,
+    isPermission,
+  }
 })
 
 // 是否没有某个权限
 export const isPermission = permission => {
-	const {isPermission} = usePermissionsStore()
-	return isPermission(permission)
+  const {isPermission} = usePermissionsStore()
+  return isPermission(permission)
 }
 
 // 添加浏览器事件,处理刷新和退出事件
 // window.addEventListener('beforeunload', () => {
-// 	localStorage.setItem('vuexState', JSON.stringify(store.state))
+//   localStorage.setItem('vuexState', JSON.stringify(store.state))
 // })
+
+// 获取用户信息
+export const fetchUserInfoObject = useBaseFetch({
+  fetchOptionFn: () => ({
+    url: 'user/getUserInfo',
+    mockProd: true,
+  }),
+  transformResponseDataFn: (responseData: IUserInfo) => {
+    const userStore = useUserStore()
+    userStore.user = responseData
+  },
+})
