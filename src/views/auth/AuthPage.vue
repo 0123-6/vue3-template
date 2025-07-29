dd1842c144b7b2e50e87259eb2245ffa172f027a
90945 <909458209@qq.com>
2025-07-29 20:31:01 +0800

    格式化代码

--- src/views/auth/AuthPage.vue
+++ src/views/auth/AuthPage.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import BaseBg from "@/components/base-bg/BaseBg.vue";
+import BaseBg from '@/components/base-bg/BaseBg.vue'
 </script>
 
 <template>
-	<div class="w-full grow">
-		<BaseBg/>
-		<div class="w-full h-full flex justify-center items-center">
-			<RouterView/>
-		</div>
-	</div>
+  <div class="w-full grow">
+    <BaseBg />
+    <div class="w-full h-full flex justify-center items-center">
+      <RouterView />
+    </div>
+  </div>
 </template>
