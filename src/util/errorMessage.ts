1b52c6d395754c7a6ac3e47b8987ec1e2d0917ed
90945 <909458209@qq.com>
2025-08-08 13:59:11 +0800

    错误全局时间改为配置式

--- src/util/errorMessage.ts
+++ src/util/errorMessage.ts
@@ -1,10 +1,11 @@
 import {ElMessage} from 'element-plus'
+import {projectConfig} from '../../project.config.ts'
 
 export default function (message: string) {
   ElMessage({
     type: 'error',
     message,
     showClose: true,
-    duration: 0,
+    duration: projectConfig.errorMessageDuration,
   })
 }
