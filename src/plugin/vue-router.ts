fa455d6acd7fb1f9fec50d460e792742370e58b3
90945 <909458209@qq.com>
2025-08-18 23:34:11 +0800

    优化vue-router.ts

--- src/plugin/vue-router.ts
+++ src/plugin/vue-router.ts
@@ -272,25 +272,24 @@ const router: Router = createRouter({
 })
 
 // 全局前置守卫
-router.beforeEach(async (to, form) => {
-  console.log(to, form)
+router.beforeEach(async (to) => {
+  console.log(to)
   // 不需要权限的页面,比如登录页面,注册页面
   if (!to.meta.requiresAuth) {
     return
   }
+  if (to.path === '/') {
+    goFirstRoute()
+    return false
+  }
 
   // 需要权限的页面
-  // 用户已经登录
+  // 用户未登录
   const userStore = useUserStore()
   if (userStore.user) {
-    if (to.path === '/') {
-      goFirstRoute()
-      return false
-    }
     return
   }
 
-  // 用户未登录
   await fetchUserInfoObject.doFetch()
   return userStore.user
     ? undefined
