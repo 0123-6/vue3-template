7639882159e8603828fa539c601f1abaa95bfd31
90945 <909458209@qq.com>
2025-09-11 20:25:35 +0800

    工具函数更新

--- src/util/message.ts
+++ src/util/message.ts
@@ -1,7 +1,28 @@
 import {ElMessage} from 'element-plus'
 import {projectConfig} from '../../project.config.ts'
 
-export default function (message: string) {
+// 成功
+export const successMessage = (message: string) => {
+  ElMessage({
+    type: 'success',
+    message,
+    showClose: true,
+    duration: projectConfig.errorMessageDuration,
+  })
+}
+
+// 警告
+export const warningMessage = (message: string) => {
+  ElMessage({
+    type: 'warning',
+    message,
+    showClose: true,
+    duration: projectConfig.errorMessageDuration,
+  })
+}
+
+// 失败
+export const errorMessage = (message: string) => {
   ElMessage({
     type: 'error',
     message,
