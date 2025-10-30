919720137d352e7bcf123031bcb5068bba787191
90945 <909458209@qq.com>
2025-10-30 23:18:33 +0800

    useBaseFetch.ts优化

--- src/util/hooks/useBaseFetch.ts
+++ src/util/hooks/useBaseFetch.ts
@@ -38,9 +38,7 @@ export const useBaseFetch = (props: IUseBaseFetch)
     abortController = new AbortController()
 
     isFetching.value = true
-    if (beforeFetchResetFn) {
-      beforeFetchResetFn()
-    }
+    beforeFetchResetFn?.()
     const fetchObject = await baseFetch({
       signal: abortController.signal,
       ...fetchOptionFn(),
@@ -52,9 +50,7 @@ export const useBaseFetch = (props: IUseBaseFetch)
       finalCallback?.(fetchObject)
       return false
     }
-    if (transformResponseDataFn) {
-      transformResponseDataFn(fetchObject.responseData?.data, fetchObject.responseData)
-    }
+    transformResponseDataFn?.(fetchObject.responseData?.data, fetchObject.responseData)
     resetIsFetching()
     finalCallback?.(fetchObject)
     return true
