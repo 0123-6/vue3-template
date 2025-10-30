3f329ae46c05af58cbcf6da354829aaf7652b60b
90945 <909458209@qq.com>
2025-10-30 21:19:37 +0800

    NotFound.vue代码格式化

--- src/views/not-found/NotFound.vue
+++ src/views/not-found/NotFound.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import router from "@/plugin/vue-router.ts";
+import router from '@/plugin/vue-router.ts'
 
 const goIndexPage = () => {
   router.push('/index')
@@ -7,10 +7,12 @@ const goIndexPage = () => {
 </script>
 
 <template>
-	<!--最外层-->
-	<div class="hpj w-full grow flex flex-col">
-		<span class="text-[30px] leading-[30px]">404</span>
-		<button @click="goIndexPage">跳转到首页</button>
-	</div>
+  <!--最外层-->
+  <div class="hpj w-full grow flex flex-col">
+    <span class="text-[30px] leading-[30px]">404</span>
+    <button @click="goIndexPage">
+      跳转到首页
+    </button>
+  </div>
 </template>
 
