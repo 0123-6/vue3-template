2fb7242f20342e6ea1aa2f288c5d43277b3103aa
90945 <909458209@qq.com>
2025-11-04 20:35:06 +0800

    BreadcrumbComp.vue组件开发

---
+++ src/views/layout-page/BreadcrumbComp.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {watchLocationPathname} from '@/util/watchLocationPathname.ts'
+import {ref} from 'vue'
+import router from '@/plugin/vue-router.ts'
+
+const menuList = ref<string[]>([])
+const getMenuList = () => {
+  queueMicrotask(() => {
+    menuList.value = router.currentRoute.value.matched.slice(1).map(route => (route.name as string))
+  })
+}
+watchLocationPathname(getMenuList)
+getMenuList()
+</script>
+
+<template>
+  <div class="flex items-center gap-x-2">
+    <span>{{ menuList[0] }}</span>
+    <template
+      v-for="(item, index) in menuList.slice(1)"
+      :key="index"
+    >
+      <span>/</span>
+      <span>{{ item }}</span>
+    </template>
+  </div>
+</template>
