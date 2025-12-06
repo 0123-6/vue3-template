8ca6ecb01ad556f8f7905af456cda85ee140c3ba
90945 <909458209@qq.com>
2025-12-06 14:36:16 +0800

    NotFound.vue优化

--- src/views/not-found/NotFound.vue
+++ src/views/not-found/NotFound.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import router from '@/plugin/vue-router.ts'
+import {goFirstRoute} from '@/plugin/vue-router.ts'
 
-const goIndexPage = () => {
-  router.push('/index')
-}
 </script>
 
 <template>
   <!--最外层-->
   <div class="hpj w-full grow flex flex-col">
     <span class="text-[30px] leading-[30px]">404</span>
-    <button @click="goIndexPage">
+    <button @click="goFirstRoute">
       跳转到首页
     </button>
   </div>
