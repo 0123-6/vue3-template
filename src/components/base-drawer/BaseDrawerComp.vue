bdb7e6174ff61eec0d98bca7b13b4780a04be7a6
90945 <909458209@qq.com>
2025-12-08 20:54:55 +0800

    BaseDrawerComp.vue优化

--- src/components/base-drawer/BaseDrawerComp.vue
+++ src/components/base-drawer/BaseDrawerComp.vue
@@ -1,9 +1,24 @@
 <script lang="ts" setup>
+import {onMounted, onUnmounted, ref} from 'vue'
+import type {OverlayScrollbars} from 'overlayscrollbars'
+import overlayScrollbar from '@/util/overlayScrollbar.ts'
+
+const divRef = ref<HTMLDivElement>(null)
+let divRefScrollbar: OverlayScrollbars
+onMounted(() => {
+  divRefScrollbar = overlayScrollbar({
+    element: divRef.value,
+    autoHide: false,
+  })
+})
+onUnmounted(() => {
+  divRefScrollbar?.destroy()
+})
 </script>
 
 <template>
   <div
-    v-scrollbar
+    :ref="(el: HTMLDivElement) => divRef = el"
     class="hpj w-full py-4 pr-6 flex flex-col"
     style="height: calc(100% - 88px)"
   >
