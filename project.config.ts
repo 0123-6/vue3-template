35eb5e8efe019425fcd5880d2699aed0e9ba8c47
90945 <909458209@qq.com>
2026-02-06 11:01:36 +0800

    baseUrl改为/vue/

--- project.config.ts
+++ project.config.ts
@@ -42,7 +42,7 @@ interface IProjectConfig {
 // 项目的配置文件
 export const projectConfig: IProjectConfig = {
   projectName: 'vue-template-one',
-  baseUrl: '/vue',
+  baseUrl: '/vue/',
   isUseCdn: true,
   apiPrefix: {
     DEFAULT: '/api/',
